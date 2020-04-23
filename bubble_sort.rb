d7938e4988ff72b61990fb41e9dd25113875fdb3
teekaytech <taofeek.olalere@feghas.com>
2020-04-23 10:06:00 +0100

    Make bubble_sort algorithm a method

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,34 +1,37 @@
-array = [22, 7, 17, 3, 50, 41, 32, 21, 11]
+def bubble_sort array
+  is_sorted = false
 
-is_sorted = false
-
-i = 0
-length = array.length - 1
-
-loop do
-  j = 0
-  is_sorted = true
-
-  break if i == length
+  i = 0
+  length = array.length - 1
 
   loop do
+    j = 0
+    is_sorted = true
 
-    break if j + i == length
-  
-    if array[j] > array[j + 1]
-      array[j], array[j+1] = array[j+1], array[j]
-      is_sorted = false
+    break if i == length
+
+    loop do
+
+      break if j + i == length
+    
+      if array[j] > array[j + 1]
+        array[j], array[j+1] = array[j+1], array[j]
+        is_sorted = false
+      end
+      break if j == length - 1
+
+      j += 1
     end
-    break if j == length - 1
 
-    j += 1
-  end
-  
-  if is_sorted 
-    break
-  end
+    if is_sorted 
+      break
+    end
 
-  i += 1
+    i += 1
+  end
+  array
 end
 
-puts array
+unsorted = [22, 7, 17, 3, 50, 41, 32, 21, 11]
+
+puts bubble_sort(unsorted)
