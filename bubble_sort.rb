4588c40c68695ad4687b261eec21a6e34e74cc28
mikearaya <mikaelaraya12@gmail.com>
2020-04-23 11:22:15 +0300

    COMPLETE UNOPTIMIZED BUBBLE SORT

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,4 +1,25 @@
+array = [9, 8, 7, 6, 5, 4, 3, 2, 1]
+
 i = 0
+length = array.length - 1
 
 loop do
+  j = 0
+
+  break if i == length
+
+  loop do
+    if array[j] > array[j + 1]
+      temp = array[j]
+      array[j] = array[j + 1]
+      array[j + 1] = temp
+    end
+    break if j == length - 1
+
+    j += 1
+  end
+
+  i += 1
 end
+
+puts array
