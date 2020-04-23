021ae3fe2c1c9480e2b7ec94e463d44dfb450d63
teekaytech <taofeek.olalere@feghas.com>
2020-04-23 10:01:18 +0100

    Optimize swap and loops

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,22 +1,31 @@
-array = [9, 8, 7, 6, 5, 4, 3, 2, 1]
+array = [22, 7, 17, 3, 50, 41, 32, 21, 11]
+
+is_sorted = false
 
 i = 0
 length = array.length - 1
 
 loop do
   j = 0
+  is_sorted = true
 
   break if i == length
 
   loop do
+
+    break if j + i == length
+  
     if array[j] > array[j + 1]
-      temp = array[j]
-      array[j] = array[j + 1]
-      array[j + 1] = temp
+      array[j], array[j+1] = array[j+1], array[j]
+      is_sorted = false
     end
     break if j == length - 1
 
     j += 1
+  end
+  
+  if is_sorted 
+    break
   end
 
   i += 1
